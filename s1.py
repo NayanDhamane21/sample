78cc6f7bbf0e88b2ee12b346a025955a3fc204a8
NayanDhamane21 <nayanvdhamane@gmail.com>
2026-04-09 14:19:47 +0530

    Update s1.py

--- s1.py
+++ s1.py
@@ -1,1 +1,14 @@
-print("nayan")
+n=int(input("enter first number")
+n2=int(input("enter second number"))
+operator=input("enter operator ( +. -. *, /, )")
+if(operator=="+"):
+  print(n+n2)
+elif(operator=="-"):
+  print(n-n2)
+elif(operator=="*"):
+  print(n*n2)
+elif(operator=="/"):
+  print(n/n2)
+else:
+  print("invalid input")
+  
