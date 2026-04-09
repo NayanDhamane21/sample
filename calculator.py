4df49c8cdcf60605188bdad5708fbce8002ea70f
NayanDhamane21 <nayanvdhamane@gmail.com>
2026-04-09 14:21:14 +0530

    Add basic calculator functionality in calculator.py

---
+++ calculator.py
@@ -0,0 +1,13 @@
+n=int(input("enter first number")
+n2=int(input("enter second number"))
+operator=input("enter operator ( +. -. *, /, )")
+if(operator=="+"):
+  print(n+n2)
+elif(operator=="-"):
+  print(n-n2)
+elif(operator=="*"):
+  print(n*n2)
+elif(operator=="/"):
+  print(n/n2)
+else:
+  print("invalid input")
